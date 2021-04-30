c9d4325aaa1fbeea55ce2d69e06ca5f9b2884f09
p0ria <p0ria@live.com>
2021-04-30 12:49:03 +0430

    material ui added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../shared/styles";
 import { theme } from "../shared/theme";
+import { removeMuiDuplicateStyle } from "../utils/dom-utils";
 
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    removeMuiDuplicateStyle()
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle theme={theme} />
