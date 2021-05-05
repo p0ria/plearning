3e2240fb2f562f7f7c9797674b64ed48eddd74dc
p0ria <p0ria@live.com>
2021-05-05 21:27:37 +0430

    framer-motion added

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import store, { RootState } from "../state/store";
 import { AppState, appState } from "../state/app/app.state";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsername } from "../state/app/app.effects";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const appState = useSelector<RootState, AppState>(state => state.app)
@@ -15,14 +16,14 @@ export default function Home() {
     dispatch(getUsername())
   }, [])
   return (
-    <>
+    <motion.div initial={{ y: -200 }} animate={{ y: 0, transition: { duration: .3 } }}>
       <Header />
       <Link href="/a"><a>Go to A by Link</a></Link>
       <a href="/a"> Go to A by anchor element</a>
       <Typography variant="h1" className="en">Hello</Typography>
       <Typography variant="h2">سلام روز بخیر <span className="fd">123</span></Typography>
       <Button variant="contained" className="en" color="primary">Hello World</Button>
-    </>
+    </motion.div>
   )
 }
 
