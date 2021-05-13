5590970219673bdb7c0d9285e077b056037e4bf1
p0ria2 <poria.mast3r@gmail.com>
2021-05-13 13:33:36 +0430

    tailwindcss added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import React, { FC, useEffect } from "react";
 import ThemeProvider from "../shared/ThemeProvider";
 import store from "../state/store";
 import { removeMuiDuplicateStyle } from "../utils/dom-utils";
+import 'tailwindcss/tailwind.css';
 
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ export default function Home() {
           ))
         }
       </Container>
-
     </motion.div>
   )
 }
